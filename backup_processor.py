64b3c02b15dd7cfdcbf97bd1277f2a5676b9ba79
Andrey Semenov <andersen5447@gmail.com>
2024-03-04 22:28:48 +0300

    init incremental backup process

--- backup_processor.py
+++ backup_processor.py
@@ -1,4 +1,5 @@
 import os
+import re
 import string
 
 import paramiko
@@ -11,7 +12,9 @@ def init_backup(hostname: string, username: string, private_key_file_path: strin
                 local_path: string, remote_path: string):
     try:
         local_files = os.listdir(local_path)
-        chunked_files = map(lambda file_name: ChunkedFile(file_path=os.path.join(local_path, file_name), file_name=file_name), local_files)
+        chunked_files = map(
+            lambda file_name: ChunkedFile(file_path=os.path.join(local_path, file_name), file_name=file_name),
+            local_files)
 
         client = RemoteConnectionClient(hostname, username, private_key_file_path)
         for chunked_file in chunked_files:
@@ -21,17 +24,15 @@ def init_backup(hostname: string, username: string, private_key_file_path: strin
             client.sftp_client.mkdir(remote_file_name)
             client.sftp_client.chdir(remote_file_name)
 
-            j = 0
-            for chunk_of_file in chunked_file.file_chunk_list:
+            for j in range(0, len(chunked_file.file_chunk_list)):
                 chunk_file_name = chunked_file.file_name[0:i] + f"_{j}" + ".txt"
                 remote_chunk_of_file = client.sftp_client.file(chunk_file_name, "a")
-                remote_chunk_of_file.write(chunk_of_file)
+                remote_chunk_of_file.write(chunked_file.file_chunk_list[j])
                 remote_chunk_of_file.flush()
-                j += 1
 
                 remote_full_path_of_chunk = os.path.join(remote_path, remote_file_name, chunk_file_name)
                 client.ssh_client.exec_command(
-                    f"setfattr --name=user.checksum --value=hash_value {remote_full_path_of_chunk}")
+                    f"setfattr --name=user.checksum --value={chunked_file.checksum_list[j]} {remote_full_path_of_chunk}")
 
     except paramiko.AuthenticationException as auth_ex:
         print("Authentication failed:", str(auth_ex))
@@ -44,3 +45,47 @@ def init_backup(hostname: string, username: string, private_key_file_path: strin
     finally:
         client.sftp_client.close()
         client.ssh_client.close()
+
+
+# tmp situation: files not added/deleted
+def incremental_backup_update(hostname: string, username: string, private_key_file_path: string,
+                              local_path: string, remote_path: string):
+    local_files = os.listdir(local_path)
+    local_chunked_files = map(
+        lambda file_name: ChunkedFile(file_path=os.path.join(local_path, file_name), file_name=file_name), local_files)
+
+    client = RemoteConnectionClient(hostname, username, private_key_file_path)
+    client.sftp_client.chdir(remote_path)
+    remote_hashes_dict = {}
+    for remote_folder_name in client.sftp_client.listdir():
+        stdin, stdout, stderr = client.ssh_client.exec_command(
+            f"cd {os.path.join(remote_path, remote_folder_name)} && getfattr -d -m 'user.checksum' ./*")
+        macthes = re.findall(r"user\.checksum=.*?\n", stdout.read().decode('utf-8'))
+        remote_hashes_dict[remote_folder_name] = (list(map(lambda el: el[el.find('=') + 2:-3], macthes)))
+
+    print(f"remote_hashes_dict - {remote_hashes_dict}")
+
+    for chunked_file in local_chunked_files:
+        i = chunked_file.file_name.find(".")
+        remote_file_name = chunked_file.file_name[0:i] + "_backup_folder"
+        remote_hashes = remote_hashes_dict[remote_file_name]
+
+        local_hashes = chunked_file.checksum_list
+        diff_chunks = []
+
+        print(f"remote hashes for file {remote_file_name} is {remote_hashes}")
+        print(f"local hashes for file {remote_file_name} is {local_hashes}")
+
+        for i in range(len(local_hashes)):
+            if local_hashes[i] != remote_hashes[i]:
+                diff_chunks.append(i)
+
+        print(f"File in differs in chunks with nums - {diff_chunks}")
+
+
+def get_remote_file_name_by_local():
+    return
+
+
+def get_local_file_name_by_remote():
+    return
--- chunk_processor.py
+++ chunk_processor.py
@@ -13,9 +13,6 @@ class ChunkedFile:
             self.file_chunk_list = list(split_string_into_chunks(contents, chunk_fix_size))
             print(self.file_chunk_list)
 
-#           where to calculate and store chunk remote name
-#             self.chunk_name_list = list(file_path + '_' + i for i in range(0, len(self.file_chunk_list)))
-
             # calculate checksum for each chunk
             self.checksum_list = list(xxhash.xxh32(chunk).digest() for chunk in self.file_chunk_list)
             print(self.checksum_list)
