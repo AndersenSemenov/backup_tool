24cd211f462794bcc90e7fde598dc5ad8b925850
Andrey Semenov <andersen5447@gmail.com>
2024-05-15 23:44:46 +0300

    recursive recover backup operation

--- recover_processor.py
+++ recover_processor.py
@@ -25,155 +25,171 @@ def recover_file(
         client.sftp_client, remote_path, os.path.join(tmp_dir, "backup_data")
     )
 
-    for tmp_local_file_folder in os.listdir(os.path.join(tmp_dir, "backup_data")):
-        os.mkdir(os.path.join(tmp_dir, "buffer", tmp_local_file_folder))
-        last_version_number = len(
-            next(os.walk(os.path.join(tmp_dir, "backup_data", tmp_local_file_folder)))[
-                1
-            ]
-        )
+    for root, subdirs, files in os.walk(os.path.join(tmp_dir, "backup_data")):
+        for subdir in subdirs:
+            if is_backup_file_folder(os.path.join(root, subdir)):
+                # os.mkdir(os.path.join(tmp_dir, "buffer", get_relative_root_path(os.path.join(tmp_dir, "backup_data"), root), subdir))
+                last_version_number = len(
+                    next(os.walk(os.path.join(tmp_dir, "backup_data", get_relative_root_path(os.path.join(tmp_dir, "backup_data"), root), subdir)))[
+                        1
+                    ]
+                )
 
-        chunks_positions = {}
-        version_number = last_version_number
-        while version_number > 0:
-            # unzip
-            with zipfile.ZipFile(
-                os.path.join(
-                    tmp_dir,
-                    "backup_data",
-                    tmp_local_file_folder,
-                    "v" + str(version_number),
-                    "archive.zip",
-                ),
-                "r",
-            ) as zip_ref:
-                zip_ref.extractall(
-                    os.path.join(
-                        os.path.join(
-                            tmp_dir,
-                            "buffer",
-                            tmp_local_file_folder,
-                            "v" + str(version_number),
+                chunks_positions = {}
+                version_number = last_version_number
+                while version_number > 0:
+                    # unzip
+                    with zipfile.ZipFile(
+                            os.path.join(
+                                tmp_dir,
+                                "backup_data",
+                                get_relative_root_path(os.path.join(tmp_dir, "backup_data"), root),
+                                subdir,
+                                "v" + str(version_number),
+                                "archive.zip",
+                            ),
+                            "r",
+                    ) as zip_ref:
+                        zip_ref.extractall(
+                            os.path.join(
+                                os.path.join(
+                                    tmp_dir,
+                                    "buffer",
+                                    get_relative_root_path(os.path.join(tmp_dir, "backup_data"), root),
+                                    subdir,
+                                    "v" + str(version_number),
+                                )
+                            )
                         )
-                    )
-                )
 
-            # iterate through extracted files and get chunk numbers
-            for backup_chunk in os.listdir(
-                os.path.join(
-                    os.path.join(
-                        tmp_dir,
-                        "buffer",
-                        tmp_local_file_folder,
-                        "v" + str(version_number),
-                    )
-                )
-            ):
-                backup_chunk_name = backup_chunk[0 : backup_chunk.find(".")]
-                if backup_chunk_name not in chunks_positions:
-                    chunks_positions[backup_chunk_name] = version_number
+                    # iterate through extracted files and get chunk numbers
+                    for backup_chunk in os.listdir(
+                            os.path.join(
+                                os.path.join(
+                                    tmp_dir,
+                                    "buffer",
+                                    get_relative_root_path(os.path.join(tmp_dir, "backup_data"), root),
+                                    subdir,
+                                    "v" + str(version_number),
+                                )
+                            )
+                    ):
+                        backup_chunk_name = backup_chunk[0: backup_chunk.find(".")]
+                        if backup_chunk_name not in chunks_positions:
+                            chunks_positions[backup_chunk_name] = version_number
 
-            version_number -= 1
+                    version_number -= 1
 
-        # deduplication process
-        version_number = last_version_number
-        dedup_all = {}
-        while version_number > 0:
-            # check if deduplication.csv exists in current version
-            path_to_current_dedup = os.path.join(
-                tmp_dir,
-                "backup_data",
-                tmp_local_file_folder,
-                "v" + str(version_number),
-                "deduplication.csv",
-            )
-            if os.path.isfile(path_to_current_dedup):
-                # parse deduplication.csv
-                dedup_structure = []
-                with open(
-                    os.path.join(
+                    # deduplication process
+                version_number = last_version_number
+                dedup_all = {}
+                while version_number > 0:
+                    # check if deduplication.csv exists in current version
+                    path_to_current_dedup = os.path.join(
                         tmp_dir,
                         "backup_data",
-                        tmp_local_file_folder,
+                        get_relative_root_path(os.path.join(tmp_dir, "backup_data"), root),
+                        subdir,
                         "v" + str(version_number),
                         "deduplication.csv",
                     )
-                ) as csv_file:
-                    csv_reader = csv.reader(csv_file, delimiter=",")
-                    for row in csv_reader:
-                        dedup_structure.append(
-                            DedupReference(
-                                int(row[0]),
-                                int(row[1]),
-                                int(row[2]),
-                                int(row[3]),
-                                row[4],
-                            )
-                        )
-                dedup_all[version_number] = dedup_structure
-
-            version_number -= 1
-
-        version_number = last_version_number
-        dedup_chunks_positions = {}
-        while version_number > 0:
-            if version_number in dedup_all:
-                dedup_structure = dedup_all[version_number]
-                for dedup_element in dedup_structure:
-                    for dedup_chunk_number in range(
-                        dedup_element.left_local, dedup_element.right_local + 1
-                    ):
-                        if dedup_chunk_number not in chunks_positions:
-                            dedup_chunks_positions[dedup_chunk_number] = (
-                                find_dedup_version(
-                                    version_number - 1,
-                                    dedup_element.left_remote
-                                    + (dedup_chunk_number - dedup_element.left_local),
-                                    dedup_all,
+                    if os.path.isfile(path_to_current_dedup):
+                        # parse deduplication.csv
+                        dedup_structure = []
+                        with open(
+                                os.path.join(
+                                    tmp_dir,
+                                    "backup_data",
+                                    get_relative_root_path(os.path.join(tmp_dir, "backup_data"), root),
+                                    subdir,
+                                    "v" + str(version_number),
+                                    "deduplication.csv",
                                 )
+                        ) as csv_file:
+                            csv_reader = csv.reader(csv_file, delimiter=",")
+                            for row in csv_reader:
+                                dedup_structure.append(
+                                    DedupReference(
+                                        int(row[0]),
+                                        int(row[1]),
+                                        int(row[2]),
+                                        int(row[3]),
+                                        row[4],
+                                    )
+                                )
+                        dedup_all[version_number] = dedup_structure
+
+                    version_number -= 1
+
+                version_number = last_version_number
+                dedup_chunks_positions = {}
+                while version_number > 0:
+                    if version_number in dedup_all:
+                        dedup_structure = dedup_all[version_number]
+                        for dedup_element in dedup_structure:
+                            for dedup_chunk_number in range(
+                                    dedup_element.left_local, dedup_element.right_local + 1
+                            ):
+                                if dedup_chunk_number not in chunks_positions:
+                                    dedup_chunks_positions[dedup_chunk_number] = (
+                                        find_dedup_version(
+                                            version_number - 1,
+                                            dedup_element.left_remote
+                                            + (dedup_chunk_number - dedup_element.left_local),
+                                            dedup_all,
+                                        )
+                                    )
+                    version_number -= 1
+
+                print(f"dedup_chunks_positions - {dedup_chunks_positions}")
+
+                with open(
+                        os.path.join(tmp_dir,
+                                     "recover_data",
+                                     get_relative_root_path(os.path.join(tmp_dir, "backup_data"), root),
+                                     subdir + ".txt"), "a"
+                ) as rf:
+                    range_val = get_final_total_number_of_chunks(
+                        os.path.join(
+                            tmp_dir,
+                            "backup_data",
+                            get_relative_root_path(os.path.join(tmp_dir, "backup_data"), root),
+                            subdir,
+                            "v" + str(last_version_number),
+                            "checksums.csv",
+                        )
+                    )
+                    for chunk_number in range(range_val):
+                        if chunk_number not in dedup_chunks_positions:
+                            chunk_location_folder = "v" + str(
+                                chunks_positions[str(chunk_number)]
                             )
-            version_number -= 1
-
-        print(f"dedup_chunks_positions - {dedup_chunks_positions}")
-
-        with open(
-            os.path.join(tmp_dir, "recover_data", tmp_local_file_folder + ".txt"), "a"
-        ) as rf:
-            range_val = get_final_total_number_of_chunks(
-                os.path.join(
-                    tmp_dir,
-                    "backup_data",
-                    tmp_local_file_folder,
-                    "v" + str(last_version_number),
-                    "checksums.csv",
-                )
-            )
-            for chunk_number in range(range_val):
-                if chunk_number not in dedup_chunks_positions:
-                    chunk_location_folder = "v" + str(
-                        chunks_positions[str(chunk_number)]
-                    )
-                    chunk_path = os.path.join(
-                        tmp_dir,
-                        "buffer",
-                        tmp_local_file_folder,
-                        chunk_location_folder,
-                        str(chunk_number) + ".txt",
-                    )
-                else:
-                    chunk_location_folder = "v" + str(
-                        dedup_chunks_positions[chunk_number].version_number
-                    )
-                    chunk_path = os.path.join(
-                        tmp_dir,
-                        "buffer",
-                        tmp_local_file_folder,
-                        chunk_location_folder,
-                        str(dedup_chunks_positions[chunk_number].chunk_number) + ".txt",
-                    )
-                rf.write(open(chunk_path).read())
+                            chunk_path = os.path.join(
+                                tmp_dir,
+                                "buffer",
+                                get_relative_root_path(os.path.join(tmp_dir, "backup_data"), root),
+                                subdir,
+                                chunk_location_folder,
+                                str(chunk_number) + ".txt",
+                            )
+                        else:
+                            chunk_location_folder = "v" + str(
+                                dedup_chunks_positions[chunk_number].version_number
+                            )
+                            chunk_path = os.path.join(
+                                tmp_dir,
+                                "buffer",
+                                get_relative_root_path(os.path.join(tmp_dir, "backup_data"), root),
+                                subdir,
+                                chunk_location_folder,
+                                str(dedup_chunks_positions[chunk_number].chunk_number) + ".txt",
+                            )
+                        rf.write(open(chunk_path).read())
 
                 # os.rmdir(os.path.join(tmp_dir, 'buffer', tmp_local_file_folder))
+            elif not os.path.exists(os.path.join(tmp_dir, "buffer", get_relative_root_path(os.path.join(tmp_dir, "backup_data"), root), subdir)):
+                os.mkdir(os.path.join(tmp_dir, "buffer", get_relative_root_path(os.path.join(tmp_dir, "backup_data"), root), subdir))
+                os.mkdir(os.path.join(tmp_dir, "recover_data", get_relative_root_path(os.path.join(tmp_dir, "backup_data"), root), subdir))
 
 
 def get_final_total_number_of_chunks(path):
@@ -229,6 +245,13 @@ def recursive_sftp_get(sftp, remote_path, local_path):
         else:
             sftp.get(remote_item, local_item)
 
+
+def is_backup_file_folder(path):
+    return "v1" in os.listdir(path)
+
+
+def get_relative_root_path(path, root):
+    return root[len(path) + 1:]
 
 class RecoverPair:
     def __init__(self, version_number, chunk_number):
