7df2b69d2e14a5fe9b2ea77acb5b82a2cb3f5373
Andrey Semenov <andersen5447@gmail.com>
2024-05-19 17:03:30 +0300

    setup cli commands

--- cli_wrapper.py
+++ cli_wrapper.py
@@ -1,5 +1,9 @@
 import click
 
+from full_backup_processor import process_full_backup
+from incremental_backup_processor import process_incremental_backup
+from recover_backup_processor import recover_file
+
 
 @click.group()
 def cli():
@@ -7,33 +11,78 @@ def cli():
 
 
 @cli.command()
-@click.option("--remote-ip-address")
-@click.option("--remote-user")
-@click.option("--private-key-file-path")
-@click.option("--local-path")
-@click.option("--remote-path")
-def init_backup(
-    remote_ip_address, remote_user, private_key_file_path, local_path, remote_path
+@click.option("--remote-ip", prompt="IP address of remote backup server machine")
+@click.option("--remote-user", prompt="Username of remote backup server machine")
+@click.option("--ssh-key-file-path", prompt="Local path to ssh key bounded with remote server")
+@click.option("--local-path", prompt="Backup local path")
+@click.option("--remote-path", prompt="Backup remote path")
+@click.option("--tmp-path", prompt="Path for temporary files created while backuping")
+def init_full_backup(
+    remote_ip,
+    remote_user,
+    ssh_key_file_path,
+    local_path,
+    remote_path,
+    tmp_path,
 ):
-    click.echo("Init backup")
-    # zip_init_backup(
-    #     remote_ip_address, remote_user, private_key_file_path, local_path, remote_path
-    # )
+    click.echo("Init first full backup")
+    process_full_backup(
+        remote_ip,
+        remote_user,
+        ssh_key_file_path,
+        local_path,
+        remote_path,
+        tmp_path,
+    )
 
 
 @cli.command()
-@click.option("--remote-ip-address")
-@click.option("--remote-user")
-@click.option("--private-key-file-path")
-@click.option("--local-path")
-@click.option("--remote-path")
+@click.option("--remote-ip", prompt="IP address of remote backup server machine")
+@click.option("--remote-user", prompt="Username of remote backup server machine")
+@click.option("--ssh-key-file-path", prompt="Local path to ssh key bounded with remote server")
+@click.option("--local-path", prompt="Backup local path")
+@click.option("--remote-path", prompt="Backup remote path")
+@click.option("--tmp-path", prompt="Path for temporary files created while backuping")
 def incremental_backup(
-    remote_ip_address, remote_user, private_key_file_path, local_path, remote_path
+    remote_ip,
+    remote_user,
+    ssh_key_file_path,
+    local_path,
+    remote_path,
+    tmp_path,
 ):
     click.echo("Incremental backup process")
-    # zip_incremental_backup_update(
-    #     remote_ip_address, remote_user, private_key_file_path, local_path, remote_path
-    # )
+    process_incremental_backup(
+        remote_ip,
+        remote_user,
+        ssh_key_file_path,
+        local_path,
+        remote_path,
+        tmp_path,
+    )
+
+
+@cli.command()
+@click.option("--remote-ip", prompt="IP address of remote backup server machine")
+@click.option("--remote-user", prompt="Username of remote backup server machine")
+@click.option("--ssh-key-file-path", prompt="Local path to ssh key bounded with remote server")
+@click.option("--remote-path", prompt="Backup remote path")
+@click.option("--tmp-path", prompt="Path for temporary files created while backuping")
+def recover(
+    remote_ip,
+    remote_user,
+    ssh_key_file_path,
+    remote_path,
+    tmp_path,
+):
+    click.echo("Incremental backup process")
+    recover_file(
+        remote_ip,
+        remote_user,
+        ssh_key_file_path,
+        remote_path,
+        tmp_path,
+    )
 
 
 if __name__ == "__main__":
