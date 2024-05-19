b4e395745ea65ae08669f68f8aaeb82132885baf
Andrey Semenov <andersen5447@gmail.com>
2024-05-19 19:43:07 +0300

    fix linter

--- setup.py
+++ setup.py
@@ -4,11 +4,7 @@ setup(
     name="JC_backup",
     version="0.1.0",
     py_modules=["JC_backup"],
-    install_requires=[
-        "Click",
-        "paramiko",
-        "xxhash"
-    ],
+    install_requires=["Click", "paramiko", "xxhash"],
     entry_points={
         "console_scripts": [
             "JC_backup = JC_backup:cli",
